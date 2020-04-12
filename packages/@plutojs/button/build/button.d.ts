6e82d13bd96c8689d1b43d50e9a6295c1b8ead95
firewood1122 <firewood_1122@163.com>
2020-04-12 14:44:22 +0800

    feat: 调整组件目录结构

--- packages/@plutojs/button/build/button.d.ts
+++
@@ -1,1 +0,0 @@
-declare function button(): void;
--- packages/@plutojs/button/story/button.story.js
+++ packages/@plutojs/button/story/button.story.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import { Button } from '@storybook/react/demo';
+import { withKnobs, text } from '@storybook/addon-knobs';
+import { withQuery } from '@storybook/addon-queryparams';
 
 export default {
   title: 'Button',
-  component: Button,
+  decorators: [withKnobs, withQuery],
   parameters: {
     backgrounds: [
       { name: '默认背景', value: '#fff', default: true },
       { name: '黑色背景', value: '#2f2f2f' },
     ],
+    query: {
+      id: '123',
+    },
   },
+};
+
+export const story1 = () => {
+  const name = text('Text', '保存');
+  return <Button>{name}</Button>;
+};
+story1.story = {
+  name: '文案按钮',
 };
 
 export const Story2 = () => (
