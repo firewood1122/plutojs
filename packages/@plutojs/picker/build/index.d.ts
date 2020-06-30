72c34dbf9556365e62d970d71a03a36113a19a61
firewood1122 <firewood_1122@163.com>
2020-06-30 19:27:38 +0800

    feat: 完善@plutojs/picker

--- packages/@plutojs/picker/build/index.d.ts
+++ packages/@plutojs/picker/build/index.d.ts
@@ -2,11 +2,13 @@ import { Component } from 'react';
 interface PickerProps {
 }
 interface PickerState {
+    showGroup: boolean;
     containerHeight: number;
 }
 export default class extends Component<PickerProps, PickerState> {
     private scrollContainer;
     state: {
+        showGroup: boolean;
         containerHeight: number;
     };
     componentDidMount(): void;
--- packages/@plutojs/picker/lib/index.tsx
+++ packages/@plutojs/picker/lib/index.tsx
@@ -3,26 +3,41 @@ const style = require('./index.less');
 
 interface GroupProps {
   containerHeight: number,
+  data: Array<{
+    text: string,
+    value: any,
+  }>,
 }
 interface GroupState {
-  itemHeight: number,
+  translateY: number,
   selected: number,
 }
 class Group extends Component<GroupProps, GroupState> {
   state = {
-    itemHeight: 0,
+    translateY: 0,
     selected: 0,
   }
 
-  private item: any = null;
+  private scrollContainer: any = null; // 可滚动容器
+  private item: any = null; // 第一个数据项对象，用于取出高度
+  private itemHeight: number = 0; // 数据项高度
+  private startY: number = 0; // 移动开始时Y座标 
 
   componentDidMount() {
-    const itemHeight = (this.item as HTMLElement).clientHeight / 2;
+    const { containerHeight } = this.props;
+    const itemHeight = (this.item as HTMLElement).clientHeight;
+    const translateY = (containerHeight - itemHeight) / 2;
+    this.itemHeight = itemHeight;
     this.setState({
-      itemHeight,
+      translateY,
     });
   }
 
+  /**
+   * 获取数据库样式名称
+   * 
+   * @param index 
+   */
   private getItemClassName = (index: number) => {
     const { selected } = this.state;
     const classNameItems = [style.groupItem];
@@ -30,30 +45,97 @@ class Group extends Component<GroupProps, GroupState> {
     return classNameItems.join(' ');
   }
 
-  render() {
-    const { containerHeight } = this.props;
-    const { itemHeight } = this.state;
-    const transfromStyle = { transform: `translate3d(0px, ${containerHeight / 2 - itemHeight}px, 0px)` };
+  /**
+   * 计算移动距离
+   */
+  private computeTranslateY = (e: React.TouchEvent) => {
+    const { data } = this.props;
+    const { translateY, selected } = this.state;
 
-    const items = [
-      {
-        text: '个人',
-        value: 1,
-      },
-      {
-        text: '企业',
-        value: 2,
-      }
-    ];
+    // 没有数据，不能移动
+    if (data.length === 0) {
+      return translateY;
+    }
+
+    // 实际移动距离
+    const currentY = e.changedTouches[0].clientY;
+    let distanceY = currentY - this.startY;
+
+    // 计算可上移最大距离
+    const maxUpDistanceY = (data.length - (selected + 1)) * this.itemHeight;
+    if (distanceY < 0 && Math.abs(distanceY) > maxUpDistanceY) distanceY = - maxUpDistanceY;
+
+    // 计算可下移最大距离
+    const maxDownDistanceY = selected * this.itemHeight;
+    if (distanceY > 0 && distanceY > maxDownDistanceY) distanceY = maxDownDistanceY;
+
+    // 可移动规围内取整
+    distanceY = parseInt((distanceY / this.itemHeight).toFixed(0)) * this.itemHeight;
+
+    return translateY + distanceY;
+  }
+
+  /**
+   * 计算移动后选中数据项
+   * 
+   * @param translateY 移动后偏移量
+   */
+  private computeSelected = (translateY: number) => {
+    const { containerHeight } = this.props;
+    const baseTranslateY = (containerHeight - this.itemHeight) / 2;
+    const selected = parseInt((Math.abs(translateY - baseTranslateY) / this.itemHeight).toFixed(0));
+    return selected;
+  }
+
+  /**
+   * 响应移动开始
+   */
+  private onTouchStart = (e: React.TouchEvent) => {
+    this.startY = e.changedTouches[0].clientY;
+  }
+
+  /**
+   * 响应移动中
+   */
+  private onTouchMove = (e: React.TouchEvent) => {
+    const translateY = this.computeTranslateY(e);
+    (this.scrollContainer as HTMLElement).style.transform = `translate3d(0px, ${translateY}px, 0px)`;
+  }
+
+  /**
+   * 响应移动结束
+   */
+  private onTouchEnd = (e: React.TouchEvent) => {
+    const translateY = this.computeTranslateY(e);
+    this.setState({
+      translateY,
+      selected: this.computeSelected(translateY),
+    });
+  }
+
+  render() {
+    const { data } = this.props;
+    const { translateY } = this.state;
+    const transfromStyle = { transform: `translate3d(0px, ${translateY}px, 0px)` }; // 选择框偏移量
+
     return (
-      <div style={transfromStyle}>
-        {
-          items.map((item, index) => (
-            <div ref={item => { if (index === 0) this.item = item; }} key={`item-${index}`} className={this.getItemClassName(index)}>
-              {item.text}
-            </div>
-          ))
-        }
+      <div
+        className={style.groupOuterContainer}
+        onTouchStart={this.onTouchStart}
+        onTouchMove={this.onTouchMove}
+        onTouchEnd={this.onTouchEnd}>
+        <div
+          className={style.groupScrollContainer}
+          style={transfromStyle}
+          ref={item => { this.scrollContainer = item; }}>
+          {
+            data.map((item, index) => (
+              <div ref={item => { if (index === 0) this.item = item; }} key={`item-${index}`} className={this.getItemClassName(index)}>
+                {item.text}
+              </div>
+            ))
+          }
+        </div>
       </div>
     );
   }
@@ -62,25 +144,52 @@ class Group extends Component<GroupProps, GroupState> {
 interface PickerProps {
 }
 interface PickerState {
+  showGroup: boolean,
   containerHeight: number,
 }
 export default class extends Component<PickerProps, PickerState> {
 
-  private scrollContainer: any = null;
+  private scrollContainer: any = null; // 可滚动容器
 
   state = {
+    showGroup: false,
     containerHeight: 0,
   }
 
   componentDidMount() {
+    // 父容器初始化完毕，再初始化子组件
     const containerHeight = (this.scrollContainer as HTMLElement).clientHeight;
     this.setState({
+      showGroup: true,
       containerHeight,
     });
   }
 
   render() {
-    const { containerHeight } = this.state;
+    const { showGroup, containerHeight } = this.state;
+    const items = [
+      {
+        text: '个人1',
+        value: 1,
+      },
+      {
+        text: '个人2',
+        value: 2,
+      },
+      {
+        text: '个人3',
+        value: 3,
+      },
+      {
+        text: '个人4',
+        value: 4,
+      },
+      {
+        text: '个人5',
+        value: 5,
+      }
+    ];
+
     return (
       <div className={`${style.container}`}>
         <div className={`${style.action}`}>
@@ -90,7 +199,11 @@ export default class extends Component<PickerProps, PickerState> {
         </div>
         <div ref={item => { this.scrollContainer = item; }} className={`${style.scroll}`}>
           <div className={style.selected}></div>
-          <Group containerHeight={containerHeight} />
+          {
+            showGroup && (
+              <Group containerHeight={containerHeight} data={items} />
+            )
+          }
         </div>
       </div>
     );
