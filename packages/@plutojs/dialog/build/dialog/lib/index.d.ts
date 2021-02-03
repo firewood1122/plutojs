8b6d8535eaa375d4df411ddbce21ca3dd4183f4d
firewood1122 <firewood_1122@163.com>
2021-02-03 15:44:27 +0800

    feat: 重新构建@plutojs/dialog

--- packages/@plutojs/dialog/build/dialog/lib/index.d.ts
+++ packages/@plutojs/dialog/build/dialog/lib/index.d.ts
@@ -1,6 +1,6 @@
 declare const _default: {
     alert: (text: string, confirm: Function, confirmText?: string) => void;
     alertCustomize: (customize: Function) => void;
-    confirm: (text: string, confirm: Function, cancelText?: string, confirmText?: string) => void;
+    confirm: (text: string, confirm: Function, cancelText?: string, confirmText?: string, cancel?: () => void) => void;
 };
 export default _default;
