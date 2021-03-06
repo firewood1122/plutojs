2feac76266709e68d41d240931d4e8ce1bf0afb3
firewood1122 <firewood_1122@163.com>
2021-03-06 11:21:49 +0800

    feat: 更新构建后产出文件

--- packages/@plutojs/dialog/build/dialog/lib/index.d.ts
+++ packages/@plutojs/dialog/build/dialog/lib/index.d.ts
@@ -1,6 +1,10 @@
+interface PropsType {
+    isMask?: boolean;
+    closeOnClickOverlay?: boolean;
+}
 declare const _default: {
     alert: (text: string, confirm: Function, confirmText?: string) => void;
-    alertCustomize: (customize: Function) => void;
+    alertCustomize: (customize: Function, options?: PropsType) => void;
     confirm: (text: string, confirm: Function, cancelText?: string, confirmText?: string, cancel?: () => void) => void;
 };
 export default _default;
