bd965221b10ba9cf459b0511fe7a2cb55b8a700b
firewood1122 <firewood_1122@163.com>
2022-05-20 15:56:55 +0800

    feat(plutojs/postcss-px2rem): 增加手动转换模式

--- packages/@plutojs/postcss-px2rem/lib/index.js
+++ packages/@plutojs/postcss-px2rem/lib/index.js
@@ -6,17 +6,35 @@ module.exports = postcss.plugin(
   "postcss-px2rem",
   (options) =>
     function (css, result) {
+      // 转换方法
+      const _parse = () => {
+        const oldCssText = css.toString();
+        const px2remIns = new Px2rem(options);
+        const newCssText = px2remIns.generateRem(oldCssText);
+        const newCssObj = postcss.parse(newCssText);
+        result.root = newCssObj;
+      };
+
+      // 默认不转换，指定文件转换
+      if (options.manual) {
+        if (
+          css.first.type === "comment" &&
+          css.first.text === "px2rem-enabled"
+        ) {
+          _parse();
+        } else {
+          return css;
+        }
+        return;
+      }
+
+      // 默认转换，指定文件不转换
       if (
         css.first.type === "comment" &&
         css.first.text === "px2rem-disabled"
       ) {
         return css;
       }
-
-      const oldCssText = css.toString();
-      const px2remIns = new Px2rem(options);
-      const newCssText = px2remIns.generateRem(oldCssText);
-      const newCssObj = postcss.parse(newCssText);
-      result.root = newCssObj;
+      _parse();
     }
 );
